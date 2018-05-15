47e1cab9d6192de86cc425748287587551949448
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-15 23:26:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ function destructivelyRemoveLastKitten(){
 function destructivelyRemoveFirstKitten(){
   kittens.shift()
 }
+
+function appendKitten(name){
+  return [...kittens, name]
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeLastKitten(name){
+  return kittens.slice(0, name.length - 1)
+}
+
+function removeFirstKitten(name){
+  return kittens.slice(1)
+}
